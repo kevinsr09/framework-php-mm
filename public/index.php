8ffa4d843b752078b5885bf10167d0317e6076ac
kevinsr09 <kevinstivenrugeles@gmail.com>
2023-11-27 12:30:34 -0500

    add response

--- public/index.php
+++ public/index.php
@@ -4,37 +4,58 @@ require_once '../vendor/autoload.php';
 
 use Rumi\Routing\Router;
 use Rumi\Http\HttpNotFoundException;
-use Rumi\Routing\Request;
+use Rumi\Http\Request;
+use Rumi\Http\Response;
 use Rumi\Server\PHPServer;
 
 $router = new Router();
 
 
-$router->get('/', function(){
-  return 'Hello get';
+$router->get('/', function(Request $request){
+  $response = new Response();
+  $response->setHeader('Content-Type', 'text/html');
+  $response->setContent(json_encode(['message' => 'Hello get']));
+  $response->status(201);
+  return $response;
 });
 
-$router->get('/hello', function(){
-  return 'Hello World';
+$router->get('/hello', function(Request $request){
+  $response = new Response();
+  $response->setHeader('Content-Type', 'text/html');
+  $response->setContent(json_encode(['message' => 'Hello']));
+  $response->status(200);
+  return $response;
 });
 
-$router->get('/hello/{name}', function(){
-  return 'Hello name';      
+$router->get('/hello/{name}', function(Request $request){
+  $response = new Response();
+  $response->setHeader('Content-Type', 'text/html');
+  $response->setContent(json_encode(['message' => 'Hello name']));
+  $response->status(200);
+  return $response;
 });
 
-$router->post('/', function(){
-  return 'Hello post';
+$router->post('/', function(Request $request){
+  $response = new Response();
+  $response->setHeader('Content-Type', 'text/html');
+  $response->setContent(json_encode(['message' => 'Hello post']));
+  $response->status(200);
+  return $response;
 });
 
+$server = new PHPServer();
 try{
-  
-  $route = $router->resolve(new Request(new PHPServer()));
-
+  $request = new Request($server);
+  $route = $router->resolve($request);
   $handler = $route->handler();
+  $response = $handler($request);
 
-  print($handler());
-  
+  $server->send_response($response);
 } catch(HttpNotFoundException $e){
-  echo $e->getMessage();
-  http_response_code(404);
+
+  $response = new Response();
+  $response->setHeader('Content-Type', 'text/plain');
+  $response->setStatus(404);
+  $response->setContent('Not found');
+  $server->send_response($response);
 }
